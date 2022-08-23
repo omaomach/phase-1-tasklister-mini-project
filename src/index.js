8f6e908603fa1d939d8d0ab42834a5a439b8779a
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-08-23 08:03:48 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createToDo(e.target.to_do.value);
+    form.reset();
+  })
+  
 });
+
+function createToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  li.textContent = todo;
+  btn.textContent = 'x';
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+
+}
+
+function handleDelete(e) {
+  console.log(e.target.parentNode.remove());
+}
